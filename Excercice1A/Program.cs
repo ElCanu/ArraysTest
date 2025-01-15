7d2243f3dfe8b87a2ce21574b19a1ef71e00837f
Sam <sam@elcanu.tv>
2025-01-14 23:15:40 -0500

    boucle pour chaque donnes saisi + ajout dans tableau

--- Excercice1A/Program.cs
+++ Excercice1A/Program.cs
@@ -4,7 +4,144 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ex1A");
+            string[] noms = new string[5];
+            string[] prenoms = new string[5];
+            float[] notesTp1 = new float[5];
+            float[] notesTp2 = new float[5];
+            float[] notesIntra = new float[5];
+            float[] notesFinal = new float[5];
+
+            bool valide;
+            
+
+            string nom;
+            string prenom;
+            float noteTp1;
+            float noteTp2;
+            float noteIntra;
+            float noteFinal;
+
+            
+
+            // Validation si besoin de redimenssionner le array
+            if (!String.IsNullOrEmpty(noms[noms.Length - 1]))
+            {
+                Array.Resize(ref noms, noms.Length * 2);
+                Array.Resize(ref prenoms, prenoms.Length * 2);
+                Array.Resize(ref notesTp1, notesTp1.Length * 2);
+                Array.Resize(ref notesTp2, notesTp2.Length * 2);
+                Array.Resize(ref notesIntra, notesIntra.Length * 2);
+                Array.Resize(ref notesFinal, notesFinal.Length * 2);
+            }
+
+            do
+            {
+                Console.WriteLine("Entrez prenom étudiant:");
+                prenom = Console.ReadLine() ?? String.Empty;
+                valide = !string.IsNullOrEmpty(prenom);
+
+                if (!valide)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Entrée invalide! Appuyez sur une touche pour recommencer...");
+                    Console.ReadKey();
+                }
+            } while (!valide);
+
+            do
+            {
+                Console.WriteLine("Entrez nom étudiant:");
+                nom = Console.ReadLine() ?? String.Empty;
+                valide = !string.IsNullOrEmpty(nom);
+
+                if (!valide)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Entrée invalide! Appuyez sur une touche pour recommencer...");
+                    Console.ReadKey();
+                }
+            } while (!valide);
+
+            do
+            {
+
+                Console.Clear();
+                Console.WriteLine("Entrez note du TP1:");
+                valide = float.TryParse(Console.ReadLine(), out noteTp1)
+                    && noteTp1 <= 100
+                    && noteTp1 > 0;
+
+                if (!valide)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Entrée invalide! Appuyez sur une touche pour recommencer...");
+                    Console.ReadKey();
+                }
+            } while (!valide);
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Entrez note du TP2:");
+                valide = float.TryParse(Console.ReadLine(), out noteTp2)
+                    && noteTp2 <= 100
+                    && noteTp2 > 0;
+
+                if (!valide)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Entrée invalide! Appuyez sur une touche pour recommencer...");
+                    Console.ReadKey();
+                }
+            } while (!valide);
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Entrez note de l'intra");
+                valide = float.TryParse(Console.ReadLine(), out noteIntra)
+                    && noteIntra <= 100
+                    && noteIntra > 0;
+
+                if (!valide)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Entrée invalide! Appuyez sur une touche pour recommencer...");
+                    Console.ReadKey();
+                }
+            } while (!valide);
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Entrez note de l'exam final");
+                valide = float.TryParse(Console.ReadLine(), out noteFinal)
+                    && noteFinal <= 100
+                    && noteFinal > 0;
+
+                if (!valide)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Entrée invalide! Appuyez sur une touche pour recommencer...");
+                    Console.ReadKey();
+                }
+            } while (!valide);
+
+            // ajout des donnes saisi dans le array (a la première place disponible)
+            for (int i = 0, taille = noms.Length; i < taille; ++i)
+            {
+                if (String.IsNullOrEmpty(noms[i]))
+                {
+                    prenoms[i] = prenom;
+                    noms[i] = nom;
+                    notesTp1[i] = noteTp1;
+                    notesTp2[i] = noteTp2;
+                    notesIntra[i] = noteIntra;
+                    notesFinal[i] = noteFinal;
+                    break;
+                }
+            }
+
         }
     }
 }
